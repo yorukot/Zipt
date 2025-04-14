35817e0751edc6ad80384afef1c6ce9976dc75b1
Yorukot <hi@yorukot.me>
2025-04-14 10:40:25 +0800

    Update backend/app/models/workspace_model.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- backend/app/models/workspace_model.go
+++ backend/app/models/workspace_model.go
@@ -29,6 +29,6 @@ type WorkspaceUser struct {
 	WorkspaceID uint64    `json:"workspace_id,string" binding:"required"`
 	Role        string    `json:"role" binding:"required"`
 	UserID      uint64    `json:"user_id,string" binding:"required"`
-	CreatedAt   time.Time `json:"created_at" gorm:"autoUpdateTime"`
-	UpdatedAt   time.Time `json:"updated_at" gorm:"autoCreateTime"`
+	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
